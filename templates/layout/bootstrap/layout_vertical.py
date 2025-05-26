1948dfc8ff5bd5d8f85cdc2bd775b5601b1ca55a
unknown <hieukhctd@gmail.com>
2025-05-26 17:21:10 +0700

    Ngày 26/05

--- templates/layout/bootstrap/layout_vertical.py
+++ templates/layout/bootstrap/layout_vertical.py
@@ -1,11 +1,18 @@
+from django.http import JsonResponse
 from django.conf import settings
 import json
-
+import os
+from pathlib import Path
 
 from web_project.template_helpers.theme import TemplateHelper
 
-menu_file_path =  settings.BASE_DIR / "templates" / "layout" / "partials" / "menu" / "vertical" / "json" / "vertical_menu.json"
 
+# menu_file_path =  settings.BASE_DIR / "templates" / "layout" / "partials" / "menu" / "vertical" / "json" / "vertical_menu.json"
+menu_file_path = Path(settings.BASE_DIR) / "templates" / "layout" / "partials" / "menu" / "vertical" / "json" / "vertical_menu.json"
+
+# json_path = os.path.join(pth_path)
+# with open(json_path, encoding='utf-8') as f:
+#     menu_file_path = json.load(f)
 """
 This is an entry and Bootstrap class for the theme level.
 The init() function will be called in web_project/__init__.py
@@ -13,6 +20,8 @@ The init() function will be called in web_project/__init__.py
 
 
 class TemplateBootstrapLayoutVertical:
+
+    @staticmethod
     def init(context):
         context.update(
             {
@@ -32,9 +41,20 @@ class TemplateBootstrapLayoutVertical:
 
         return context
 
+    @staticmethod
     def init_menu_data(context):
-        # Load the menu data from the JSON file
-        menu_data = json.load(menu_file_path.open()) if menu_file_path.exists() else []
+        menu_data = []
+        if menu_file_path.exists():
+            with menu_file_path.open(encoding='utf-8') as f:
+                try:
+                    menu_data = json.load(f)
+                except json.JSONDecodeError as e:
+                    # Log hoặc xử lý lỗi nếu cần
+                    print(f"Lỗi đọc JSON: {e}")
 
-        # Updated context with menu_data
         context.update({"menu_data": menu_data})
+        # # Load the menu data from the JSON file
+        # menu_data = json.load(menu_file_path.open()) if menu_file_path.exists() else []
+
+        # # Updated context with menu_data
+        # context.update({"menu_data": menu_data})
